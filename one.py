7468720cc82b2c11a5ba930440213a62935803c5
David Aronson <daronson@kewforest.org>
2017-10-22 20:21:37 -0400

    one working

---
+++ one.py
@@ -0,0 +1,18 @@
+def daynumtoname(num):
+    if num == 0:
+        return "Sunday"
+    elif num == 1:
+        return "Monday"
+    elif num == 2:
+        return "Tuesday"
+    elif num == 3:
+        return "Wednesday"
+    elif num == 4:
+        return "Thursday"
+    elif num == 5:
+        return "Friday"
+    elif num == 6:
+        return "Saturday"
+    else:
+        return "Please enter an integer between 0 and 6."
+print(daynumtoname(int(input("Please enter an integer between 0 and 6."))))    
