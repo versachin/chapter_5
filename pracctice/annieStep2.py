95f856a22ce042ffe0e1854dffb925f2a61b4b4b
justishawkins <32522747+justishawkins@users.noreply.github.com>
2017-10-25 08:34:45 -0400

    practicve

---
+++ pracctice/annieStep2.py
@@ -0,0 +1,15 @@
+print("whats yo age")
+age = float (input())
+if age <4:
+    print("Not in School")
+    
+elif 4<=age and age <5:
+    print("Pre School")
+elif 5<age<10:
+    print("Lower School")
+elif 10<age<13:
+    print("middle school")
+elif 13<age<18:
+    print("upper school")
+elif age>18:
+    print("wasdads?")
---
+++ pracctice/anniestef.py
@@ -0,0 +1,11 @@
+print("Yo what's yo name")
+name=input()
+if name == "Leo" or name== "Mikael":
+    print("Group 2")
+elif name == "Justis" or name == "David":
+    print("Group 3")
+elif name == "Kayla" or name == "Gen":
+    print("Group 4")
+else:
+    print("IDK")
+ 
